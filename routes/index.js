e7637b84f864f1e528f1018a4347b5da98b6b140
Martha <you@example.com>
2023-02-24 13:41:42 +0300

    added path to inex.html directory

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../static/index.html'));
+    res.sendFile(path.join(__dirname, '../File/index.html'));
 });
 
 module.exports = router;
