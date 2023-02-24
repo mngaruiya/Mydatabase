a17fa2f957c2f619207aaa12821ea72fb0289965
Martha <you@example.com>
2023-02-24 12:10:54 +0300

    created new file

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, '../static/index.html'));
+});
+
+module.exports = router;
+
